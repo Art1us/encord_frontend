a95913c254abf9fa01f93beb56fe5081743028f5
Artur <artur0503915024@gmail.com>
2023-07-18 13:48:51 +0300

    fin footer

--- src/widgets/Footer/ui/Footer.tsx
+++ src/widgets/Footer/ui/Footer.tsx
@@ -1,5 +1,10 @@
 import styles from "./Footer.module.scss"
 
 export function Footer() {
-    return <footer className={styles.container}>Footer</footer>
+    return (
+        <footer className={styles.container}>
+            <div className={styles.border} />
+            <p className={styles.copyright}>© 2022 Encord. All rights reserved.</p>
+        </footer>
+    )
 }
