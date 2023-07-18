941eeae68b4c447f366c2b7cfecb072985fbe986
Artur <artur0503915024@gmail.com>
2023-07-18 13:42:16 +0300

    fin header

--- src/widgets/Header/ui/Header.tsx
+++ src/widgets/Header/ui/Header.tsx
@@ -1,5 +1,15 @@
+import { Button, ButtonVariant } from "shared/ui/Button/Button"
 import styles from "./Header.module.scss"
+import { Logo } from "shared/assets/Logo"
 
 export function Header() {
-    return <header className={styles.container}>Header</header>
+    return (
+        <header className={styles.container}>
+            <Logo />
+            <div className={styles.buttons}>
+                <Button>Log In</Button>
+                <Button variant={ButtonVariant.FILLED}>Get Started</Button>
+            </div>
+        </header>
+    )
 }
